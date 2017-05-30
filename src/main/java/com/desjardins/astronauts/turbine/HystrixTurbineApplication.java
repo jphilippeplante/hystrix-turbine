453f3d240cabca7f3b50e30350516b96bf111fad
Jean-Philippe Plante <jphilippeplante@gmail.com>
2017-05-30 13:26:34 -0400

    Initial commit

---
+++ src/main/java/com/desjardins/astronauts/turbine/HystrixTurbineApplication.java
@@ -0,0 +1,16 @@
+package com.desjardins.astronauts.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+
+@EnableHystrixDashboard
+@EnableTurbineStream
+@SpringBootApplication
+public class HystrixTurbineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixTurbineApplication.class, args);
+    }
+}
